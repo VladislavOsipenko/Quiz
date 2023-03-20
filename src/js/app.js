578a7dd3230fde92c164a0d4c0506ea9a493393a
Vladislav Osipenko <vladislavosipenko584@gmail.com>
2023-03-21 00:52:18 +0200

    add media

--- src/js/app.js
+++ src/js/app.js
@@ -12,8 +12,37 @@ document.addEventListener('DOMContentLoaded', () => {
             nextEl: '.winner__arrows-right',
             prevEl: '.winner__arrows-left',
           },
-        grabCursor: false,
+        grabCursor: true,
         slidesPerView: 1,
     });
+
+    const links = document.querySelectorAll('a');
+    const buttons = document.querySelectorAll('button');
+
+    function addPreventDefault(arg) {
+        arg.forEach(elem => {
+            elem.addEventListener('click', (e) => {
+                e.preventDefault();
+            })
+        })
+    }
+    addPreventDefault(links);
+    addPreventDefault(buttons);
+    
+    const menu = document.querySelector('.header__menu'),
+        menuItem = document.querySelectorAll('.header__li'),
+        hamburger = document.querySelector('.header__hamburger');
+    
+        hamburger.addEventListener('click', () => {
+            hamburger.classList.toggle('header__hamburger_active');
+            menu.classList.toggle('header__menu_active');
+        });
+    
+        menuItem.forEach(item => {
+            item.addEventListener('click', () => {
+                hamburger.classList.toggle('header__hamburger_active');
+                menu.classList.toggle('header__menu_active');
+            })
+        });
 })
 
